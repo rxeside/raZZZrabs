c8920fe2b9783578ccc56f07d7ef2273d560845f
rxeside <denis.leuhin@ispring.institute>
2023-09-22 09:18:01 +0300

    struct

--- 01/main.ts
+++ 01/main.ts
@@ -1,34 +1,100 @@
-type StylingSlides = {
-    samples: 'graphic';
-    colors: string,
-    styles: string,
-    images: 'image',
+type TextStyleType = {
+    isBold: boolean,
+    isItalic: boolean,
+    isUnderline: boolean,
+    color: string,
+    opacity: number,
+    fontSize: number,
+    fontFamily: string,
 }
 
-type CreatingObj = {
-    pasteText: 'text',
-    pasteFugire: 'graphic',
-    pasteImage: 'image',
+type VerticalAlign = {
+    top: boolean,
+    center: boolean,
+    bottom: boolean,
 }
 
-type WorkWithText = {
-    changingFont: 'text',
-    changingStyle: 'text',
-    changingSize: BigInt,
-    changingColor: string,
-    changingAlignment: string,
+type HorizontalAlign = {
+    left: boolean,
+    center: boolean,
+    right: boolean,
 }
 
-type Exporting = {
-    exportFile: File,
+type TextBlock = Array<TextStyleType|VerticalAlign|HorizontalAlign>
+
+type ImageBlock = {
+    data: string,
 }
 
-type Importing = {
-    importFile: File,
+type PrimitiveElement = {
+    circle: boolean,
+    triangle: boolean,
+    rectangle: boolean,
 }
 
-type Editor = Array<StylingSlides|CreatingObj|WorkWithText|Exporting|Importing>
+type PrimitivesType = {
+    element: PrimitiveElement,
+    color: string,
+}
 
-type PopUpWindow = {
-    deletingObj: что тут?,
+type ShapeBlock = {
+    primitives: PrimitivesType,
+}
+
+
+
+type ElementContent = Array<TextBlock|ImageBlock|ShapeBlock>
+
+type BorderStyleType = {
+    solid: boolean,
+    doted: boolean,
+    dashed: boolean,
+    double: boolean,
+    inset: boolean,
+    outset: boolean,    
+}
+
+type BorderType = {
+    color: string,
+    width: number,
+    opacity: number,
+    round: number,
+    borderStyle: BorderStyleType,
+}
+
+type Dot = {
+    x: number,
+    y: number,
+}
+
+type SlideElement = {
+    content: ElementContent,
+    startDot: Dot,
+    width: number,
+    height: number,
+    opacity: number,
+    scale: number,
+    border: BorderType,
+    elementID: number,
+}
+
+type Slide = {
+    slideObjects: SlideElement[],
+    slideID: number,
+}
+
+type StateCheck = {
+    slideNumber: number,
+    elementNumber: number,
+}
+
+type SlideHistory = {
+    lastSave: Slide[],
+}
+
+type Page = {
+    slide: Slide[],
+    slideHistory: SlideHistory,
+    stateCheck: StateCheck,
+    title: string,
 }
