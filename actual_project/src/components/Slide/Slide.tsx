5e77d437c0199ce8342416dd5cada4bf76a2095b
monkey <canbe2227@gmail.com>
2023-11-17 11:21:21 +0300

    fix back

--- actual_project/src/components/Slide/Slide.tsx
+++ actual_project/src/components/Slide/Slide.tsx
@@ -21,6 +21,16 @@ function Slide({ slide, className }: SlideProps) {
     return ''
   }
 
+  function hex2rgb(c: string, o: number) {
+    const bigint = parseInt(c.substring(1), 16)
+    const r = (bigint >> 16) & 255
+    const g = (bigint >> 8) & 255
+    const b = bigint & 255
+    const x = o * 100
+
+    return 'rgb(' + r + ' ' + g + ' ' + b + ' / ' + x + '%' + ')'
+  }
+
   function setBackground(slide: TSlide, style: CSSProperties) {
     if (
       slide.slideBackground.color.hex &&
@@ -31,8 +41,10 @@ function Slide({ slide, className }: SlideProps) {
       slide.slideBackground.color.opacity &&
       slide.slideBackground.color.hex
     ) {
-      style.opacity = slide.slideBackground.color.opacity
-      style.backgroundColor = slide.slideBackground.color.hex
+      style.backgroundColor = hex2rgb(
+        slide.slideBackground.color.hex,
+        slide.slideBackground.color.opacity,
+      )
     }
     return style
   }
